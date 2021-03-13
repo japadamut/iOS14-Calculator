b0a4850bc40f6297d09774f4335af1feb036fee0
Juan A. Pujante Adamut <jap.adamut@outlook.com>
2021-03-13 02:47:15 +0100

    Fix sceneWindow in setupView with parameter

---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Juan A. Pujante Adamut on 13/03/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
